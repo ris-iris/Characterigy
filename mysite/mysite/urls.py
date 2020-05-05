bf9ded80abde569006f419082a7f021b4c8f4e91
ris-iris <sagitova.ma@phystech.edu>
2020-05-05 21:21:38 +0300

    change urls

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('users/', include('mainapp.urls')),
+    path('', include('mainapp.urls')),
     path('admin/', admin.site.urls),
 ]
 
